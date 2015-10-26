d453c28ef999fd46d6be29b0172db4b83606281c
Chocolayte <baudin.pro@gmail.com>
2015-10-26 20:31:41 +0100

    Hide popup on click

--- NPush/Views/PopupView.xaml.cs
+++ NPush/Views/PopupView.xaml.cs
@@ -1,10 +1,22 @@
-﻿namespace NPush.Views
+﻿using System.Windows;
+using System.Windows.Input;
+using NPush.ViewModels;
+
+namespace NPush.Views
 {
     public partial class PopupView
     {
         public PopupView()
         {
             InitializeComponent();
+        }
+
+        private void PopupView_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var dataContext = this.DataContext as PopupViewModel;
+
+            if (dataContext != null)
+                dataContext.IsOpen.Value = false;
         }
     }
 }
