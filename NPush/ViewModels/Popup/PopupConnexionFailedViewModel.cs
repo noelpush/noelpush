8dc169618a1db80b7aa60dd9d551c1aa74c43c85
Chocolayte <baudin.pro@gmail.com>
2015-11-11 14:25:43 +0100

    Add PopupConnexionFailedViewModel

---
+++ NPush/ViewModels/Popup/PopupConnexionFailedViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NoelPush.ViewModels.Popup
+{
+    public class PopupConnexionFailedViewModel : PopupViewModel
+    {
+        public PopupConnexionFailedViewModel()
+        {
+            this.Height = 118;
+            this.Width = 323;
+        }
+    }
+}
