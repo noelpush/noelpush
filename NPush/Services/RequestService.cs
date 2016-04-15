c992fe1a411dadcedde163b7326946a6e37b9472
Chocolayte <baudin.pro@gmail.com>
2016-04-15 11:08:03 +0200

    Create RequestService

---
+++ NPush/Services/RequestService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NoelPush.Services
+{
+    public static class RequestService
+    {
+        public static async Task<string> SendRequest(string url, Dictionary<string, string> values)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var parameters = new FormUrlEncodedContent(values);
+                    var response = await client.PostAsync(url, parameters);
+                    var contents = await response.Content.ReadAsStringAsync();
+                    return contents;
+                }
+            }
+            catch (Exception e)
+            {
+                LogManager.GetCurrentClassLogger().Error(e.Message);
+            }
+
+            return string.Empty;
+        }
+    }
+}
