46d91339c56838057f2fd5c2d709aed83ee4e1ae
Chocolayte <baudin.pro@gmail.com>
2016-04-19 11:33:27 +0200

    Update the year 2016 in the AssemblyInfo

--- NPush/Properties/AssemblyInfo.cs
+++ NPush/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NoelPush")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
